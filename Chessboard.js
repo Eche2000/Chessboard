22f58234655193bf1ca2f2d3bfcf031403988c44
Eche <echezona2000@gmail.com>
2022-07-28 20:27:04 +0100

    initial commit

--- Chessboard.js
+++ Chessboard.js
@@ -1,3 +1,30 @@
-function chessboard (n){
-    
+function chessboard(n) {
+	let result = '';
+
+	// loop for the rows
+	for (let i = 0; i <= n; i++) {
+		//for loop for the columns
+		for (let j = 0; j <= n; j++) {
+			//checking for odd numbers
+			if (i % 2 == 0) {
+				if (j % 2 == 0) {
+					result += ' ';
+				} else {
+					result += '#';
+				}
+
+				//checking for even numbers
+			} else {
+				if (j % 2 == 0) {
+					result += '#';
+				} else {
+					result += ' ';
+				}
+			}
+		}
+		result += '\n';
+	}
+	return result;
 }
+
+console.log(chessboard(8));
